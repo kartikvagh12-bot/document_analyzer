247929fe07eebda4881bb5f86233c7d5664ebaaf
Kartik Vagh <kartikvagh12@gmail.com>
2026-03-06 20:35:28 +0530

    Update import for RecursiveCharacterTextSplitter
    Replace import from langchain.text_splitter with langchain_text_splitters.

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import streamlit as st
+
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 
